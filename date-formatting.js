79942536674b46a90c7e293665911115746f4111
sanketk07 <sanketskumar@gmail.com>
2018-03-21 14:16:22 -0400

    Editing date formater

--- date-formatting.js
+++ date-formatting.js
@@ -13,7 +13,7 @@ function formatDate(userDate) {
     dt_month = ""+ 0 + dt_date;
     console.log("Month: "+ dt_month);
   
-  return "" + dt_year + dt_month + dt_date;
+  return "" + dt_year + (dt_month<10? '0'+dt_month: ''+dt_month) + (dt_date<10? '0'+dt_date: ''+dt_date);
 }
 
 console.log(formatDate("1/3/2014"));
