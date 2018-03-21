45ac4fda51b842b27a841bc10bc4d0b10cca4957
sanketk07 <sanketskumar@gmail.com>
2018-03-21 14:06:55 -0400

    Adding date format convertor

---
+++ date-formatting.js
@@ -0,0 +1,19 @@
+function formatDate(userDate) {
+  // format from M/D/YYYY to YYYYMMDD
+  var dt = new Date(userDate);
+  var dt_date = dt.getDate();
+  var dt_month = dt.getMonth()+1;
+  var dt_year = dt.getFullYear();
+  
+  if(dt_date.size==1)
+    dt_date = ""+ 0 + dt_date;
+    console.log("Date: "+ dt_date);
+  
+  if(dt_month.size==1)
+    dt_month = ""+ 0 + dt_date;
+    console.log("Month: "+ dt_month);
+  
+  return "" + dt_year + dt_month + dt_date;
+}
+
+console.log(formatDate("1/3/2014"));
