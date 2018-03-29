3c5ea5998d41c835db00f7518955efd2f6d91123
sanketk07 <sanketskumar@gmail.com>
2018-03-28 20:06:32 -0400

    Added switch exercise in conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -106,3 +106,44 @@ function update(bgColor, textColor) {
 select.onchange = function() {
   ( select.value === 'black' ) ? update('black','white') : update('white','black');
 }
+
+
+//switch exercise
+var select = document.querySelector('select');
+var html = document.querySelector('.output');
+
+select.onchange = function() {
+  var choice = select.value;
+
+  
+  switch(choice){
+  case 'white':
+  update('white', 'black');
+  break;
+
+case 'black':
+  update('black', 'white');
+  break;
+
+case 'purple':
+  update('purple', 'white');
+  break;
+
+case 'yellow':
+  update('yellow', 'black');
+  break;
+
+case 'psychedelic':
+  update('psychedelic', 'white');
+  break;
+
+
+default:
+  update('#f5f9fa', 'black');
+}
+}
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
