f2d58306dd7e6b899aed4895ac0bad83a807a83e
sanketk07 <sanketskumar@gmail.com>
2018-07-23 18:24:58 -0400

    Adding array methods

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -18,7 +18,7 @@ console.log(alphabets.length); //3
 Methods:
 //*****************************
 
-//Array.from()
+//Array.from() - creates a new, shallow-copied Array instance from an array-like or iterable object
 
 console.log(Array.from('test')); //Array ["f", "o", "o"]
 console.log(Array.from([1, 2], z => z*z)); //Array [1, 4]
@@ -43,11 +43,27 @@ console.log(f(1, 2, 3)); // [1, 2, 3]
 console.log(Array.from({length: 5}, (v, i) => i)); // Array [0, 1, 2, 3, 4]
 
 
-//Array.isArray()
+//Array.isArray() - checks if the passed value is an array
 console.log(Array.isArray([1, 2, 3]));  // true
 console.log(Array.isArray({foo: 123})); // false
 console.log(Array.isArray('foobar'));   // false
 console.log(Array.isArray(undefined));  // false
+
+
+//Array.of() - creates a new Array instance with a variable number of arguments
+console.log(Array.of(5)); // Array [5] 
+console.log(Array.of(1, 2)); // Array [1, 2]
+
+console.log(Array(5)); // Array [ , , , , ]
+console.log(Array(1, 2)); // Array [1, 2]
+
+
+// Array.prototype.concat() - merges two arrays and returns a NEW array (does NOT change the existing arrays)
+var arr1 = ['a', 'b'];
+var arr2 = ['y', 'z'];
+console.log(arr1.concat(arr2)); // Array ["a", "b", "y", "z"]
+console.log(arr2.concat(arr1)); //Array ["y", "z", "a", "b"]
+
 
 
 
