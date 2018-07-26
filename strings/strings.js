688e9e6f5c79a4ffe7d4ee806a4192b58d07dc35
sanketk07 <sanketskumar@gmail.com>
2018-07-25 23:13:02 -0700

    Added fromCharCode

--- strings/strings.js
+++ strings/strings.js
@@ -12,3 +12,7 @@ console.log(someString.length); //4
 
 //Methods:
 //*****************************
+//String.fromCharCode() - returns a string created from the specified sequence of UTF-16 code units
+console.log(String.fromCharCode(65, 66, 67));  // "ABC"
+console.log(String.fromCharCode(0x2014))      // "—"
+console.log(String.fromCharCode(0x12014))      // also "—" (the digit 1 is truncated and ignored)
