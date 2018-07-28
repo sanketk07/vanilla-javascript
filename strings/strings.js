2071074487336075e8f2fd4d3b255be85f2438c9
sanketk07 <sanketskumar@gmail.com>
2018-07-28 02:15:50 -0400

    Added charAt()

--- strings/strings.js
+++ strings/strings.js
@@ -31,3 +31,17 @@ console.log(String.fromCodePoint(-1));       // RangeError
 console.log(String.fromCodePoint(3.14));     // RangeError
 console.log(String.fromCodePoint(3e-2));     // RangeError
 console.log(String.fromCodePoint(NaN));      // RangeError
+
+//String.prototype.charAt() - returns a new string consisting of the single UTF-16 code unit located at the specified offset into the string
+var anyString = 'Brave new world';
+console.log("The character at index 0   is '" + anyString.charAt()   + "'"); // B - No index was provided, used 0 as default
+console.log("The character at index 0   is '" + anyString.charAt(0)   + "'"); // The character at index 0   is 'B'
+console.log("The character at index 1   is '" + anyString.charAt(1)   + "'"); // The character at index 1   is 'r'
+console.log("The character at index 2   is '" + anyString.charAt(2)   + "'"); // The character at index 2   is 'a'
+console.log("The character at index 3   is '" + anyString.charAt(3)   + "'"); // The character at index 3   is 'v'
+console.log("The character at index 4   is '" + anyString.charAt(4)   + "'"); // The character at index 4   is 'e'
+console.log("The character at index 999 is '" + anyString.charAt(999) + "'"); // The character at index 999   is ''
+
+
+
+
